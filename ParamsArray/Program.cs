e9fcfa0151696524b00e6cb56dead7ae4f95254b
abenteuerzeit <amroz.muzyka@gmail.com>
2022-08-25 12:09:10 +0200

    Implement examples

--- ParamsArray/Program.cs
+++ ParamsArray/Program.cs
@@ -9,6 +9,19 @@ namespace ParamsArray
             //Console.WriteLine(Utils.Sum(null));
             //Console.WriteLine(Utils.Sum());
             Console.WriteLine(Utils.Sum(10, 9, 8, 7, 6, 5, 4, 3, 2, 1));
+
+            //Given a choice between
+            //a method that takes optional parameters and
+            //a method that takes a parameter list,
+            //the C# compiler uses the method that takes optional parameters.
+            Console.WriteLine(Utils.Sum(2, 4, 6, 8));
+            Console.WriteLine(Utils.Sum(2, 4, 6));
+
+            //more arguments are provided than the method that takes
+            //optional parameters specifies, so the compiler generated code that calls
+            //the method that takes a parameter array.
+            Console.WriteLine(Utils.Sum(2, 4, 6, 8, 10));
+
         }
 
         static void Main()
--- ParamsArray/Util.cs
+++ ParamsArray/Util.cs
@@ -6,6 +6,7 @@ namespace ParamsArray
     {
         public static int Sum(params int[] paramList)
         {
+            Console.WriteLine("Using paramter list");
             if (paramList == null)
             {
                 throw new ArgumentNullException("Utils.Sum: null parameter list");
@@ -22,6 +23,12 @@ namespace ParamsArray
                 sumTotal += i;
             }
             return sumTotal;
+        }
+
+        public static int Sum(int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0)
+        {
+            Console.WriteLine("Using optional parameters");
+            return param1 + param2 + param3 + param4;
         }
     }
 }
